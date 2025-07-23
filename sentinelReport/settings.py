8f6af20005128b8af986299e4327a0e721251e71
somtoval <ogbesomto042@gmail.com>
2025-07-23 08:15:40 +0100

    added csrf origin

--- sentinelReport/settings.py
+++ sentinelReport/settings.py
@@ -92,3 +92,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR / "static")]
 STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://crime-report-system.onrender.com',
+]
